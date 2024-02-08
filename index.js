c4453f93659ca4222b534ff88524ae1972f5ad85
jacob-kasaso <jacobkasaso@gmail.com>
2024-02-08 12:10:26 +0300

    complited assigniment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper,!';
+"High" + "five!";
+"we" + '' + `can` + 'concat' + `enate` + "as many strings" + 'as our heart' + `desire.`;
+const myString = 'template literal';
+const myNumber = 10;
+const myBoolean = false;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+'Edsger Dijkstra'[0];
+'Edsger Dijkstra'.charAt(0);
+'Edsger Dijkstra'.length;
+'Edsger Dijkstra'[15];
+'Edsger Dijkstra'[14];
+'Edsger Dijkstra'.slice();
+'Edsger Dijkstra'.slice(7);
+'Edsger Dijkstra'.slice(0, 3);
+currentUser.slice(0, 1);
+"const currentUser = 'Grace Hopper';";
+"const currentUser = 'Grace Hopper';".length;
+
